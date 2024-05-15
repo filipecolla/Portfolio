560a098e5d1b3d9c5e2cc47cbf552f7584a97dd7
Filipe Colla <filipe10colla@gmail.com>
2024-05-15 09:52:00 -0300

    Portfolio nearing completion | 96% finished 2024.1 | DATA.js Cards fix

--- src/data/data.js
+++ src/data/data.js
@@ -4,7 +4,7 @@ export const data = [
     img: 'https://i.postimg.cc/DwVQgRck/imagem-2024-05-14-151403328.png',
     title: "Service Desk",
     desc: "This project posed a challenge wherein our team undertook the task of developing a service desk utilizing Type, JavaScript, and React. Within this context, I emerged as one of the primary contributors to the backend development.",
-    link: "",
+    link: "https://github.com/Sync-FATEC/API-NEFRO/tree/main",
     git: "https://github.com/Sync-FATEC/API-DESK",
   },
   {
@@ -12,7 +12,7 @@ export const data = [
     img: 'https://i.postimg.cc/gkMFLqyZ/imagem-2024-05-14-150753345.png',
     title: "Nefro API",
     desc: "This was my debut academic project, where our team was challenged to create a website for mothers advocating for chronic kidney disease awareness in Brazil. I played a key role in backend development, focusing on login and blog functionalities.",
-    link: "",
+    link: "https://github.com/Sync-FATEC/API-NEFRO/tree/main",
     git: "https://github.com/Sync-FATEC/API-NEFRO/tree/main",
   },
   {
